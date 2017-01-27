b8604b6bf0efdb0dc71d2f6a81de5a36c95a3814
Diego Ballesteros <diballesterosc@gmail.com>
2017-01-26 20:05:22 -0500

    Chapter 1
    Examples and Practice

---
+++ YDKJS_Chapter1_Practice.js
@@ -0,0 +1,26 @@
+    /* 
+        Practice
+                    */
+    
+    const TAX_RATE = 0.08;
+    const phonePrice = 100;
+    const accPrice = 20;
+    const threshold = 200;
+    
+    var bank_account_balance = 1000;
+    var totalPrice = 0;
+    var individualPurchase = 0;
+    
+    function calculateTax(amt){
+        amt = amt + (amt * TAX_RATE)
+    }
+    
+    // Outer function to calculate phone purchases
+    while(bank_account_balance >= totalPrice){
+        
+        
+        //Inner Loop for phone accessories
+        while(threshold >= individualPurchase){
+            
+        }
+    }
